1c9333f45396b8910c61e42c69c50c9093b63bde
Trung Nguyen Thien <ngthientrung@gmail.com>
2023-03-13 00:28:26 +0700

    UPDATED

--- chrome_xcontent/main.js
+++ chrome_xcontent/main.js
@@ -1,11 +1,6 @@
 (async function initPopupWindow() {
-    document.getElementById("myButton").addEventListener("click", function () {
-        alert("Hello, World!");
-    });
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url;
-        alert(url);
-        // use `url` here inside the callback because it's asynchronous!
+        $("#url").text(url);
     });
-
 })();
